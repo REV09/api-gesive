b222a92b9f7029e795bfa92058e2f48b9aba3a43
REV09 <hector.morelos.607@gmail.com>
2023-05-20 23:06:18 -0600

    cambio de base de datos

--- config/db.py
+++ config/db.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
-
+import sqlalchemy
+import ssl
 
 def conexionDb():
 
     '''
+    mysql+pymysql://76bcbcdd423c7195:3d9356e09f9e6290e0e874618db1d1@primary.aseguradorabd--9x2lmpbqp2qf.addon.code.run:40594/2d6206eac7c9
     '''
 
-    motorBd = create_engine("mysql+pymysql://e212849251743278:4f72da1965aec2f114e7b7c968e33a"
-                            "@primary.aseguradorabd--9x2lmpbqp2qf.addon.code.run:3306/864c14fd73b9")
+    motorBd = create_engine("mysql+pymysql://76bcbcdd423c7195:3d9356e09f9e6290e0e874618db1d1"
+                            "@primary.aseguradorabd--9x2lmpbqp2qf.addon.code.run:40594/2d6206eac7c9", echo=True,   connect_args={'ssl': {'activate': True}})
     conexion = motorBd.connect()
     return conexion
 
@@ -17,6 +19,6 @@ def motor():
     '''
     '''
 
-    motorBd = create_engine("mysql+pymysql://e212849251743278:4f72da1965aec2f114e7b7c968e33a"
-                            "@primary.aseguradorabd--9x2lmpbqp2qf.addon.code.run:3306/864c14fd73b9")
+    motorBd = create_engine("mysql+pymysql://76bcbcdd423c7195:3d9356e09f9e6290e0e874618db1d1"
+                            "@primary.aseguradorabd--9x2lmpbqp2qf.addon.code.run:40594/2d6206eac7c9", echo=True,   connect_args={'ssl': {'activate': True}})
     return motorBd
