588eb94fb371275c58ea3e6410672b859ec8a0d6
REV09 <hector.morelos.607@gmail.com>
2023-06-03 14:07:50 -0600

    cambiado usuario y contraseña

--- config/db.py
+++ config/db.py
@@ -8,7 +8,7 @@ def conexionDb():
     mysql+pymysql://76bcbcdd423c7195:3d9356e09f9e6290e0e874618db1d1@primary.aseguradorabd--9x2lmpbqp2qf.addon.code.run:40594/2d6206eac7c9
     '''
 
-    motorBd = create_engine("mysql+pymysql://76bcbcdd423c7195:3d9356e09f9e6290e0e874618db1d1"
+    motorBd = create_engine("mysql+pymysql://apiuser:BDdsr198709-"
                             "@primary.aseguradorabd--9x2lmpbqp2qf.addon.code.run:40594/2d6206eac7c9", echo=True,   connect_args={'ssl': {'activate': True}})
     conexion = motorBd.connect()
     return conexion
@@ -19,6 +19,6 @@ def motor():
     '''
     '''
 
-    motorBd = create_engine("mysql+pymysql://76bcbcdd423c7195:3d9356e09f9e6290e0e874618db1d1"
+    motorBd = create_engine("mysql+pymysql://apiuser:BDdsr198709-"
                             "@primary.aseguradorabd--9x2lmpbqp2qf.addon.code.run:40594/2d6206eac7c9", echo=True,   connect_args={'ssl': {'activate': True}})
     return motorBd
